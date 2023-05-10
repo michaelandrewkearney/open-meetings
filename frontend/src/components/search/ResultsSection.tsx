eee52fc8d642943fc596c669e95250c581152cc5
ezhangy <elizabeth_zhang1@brown.edu>
2023-05-10 02:13:20 -0400

    updated results info message again

--- frontend/src/components/search/ResultsSection.tsx
+++ frontend/src/components/search/ResultsSection.tsx
@@ -55,8 +55,8 @@ export default function ResultSection({
       />
       <main id={styles["results"]} aria-label="Search results">
         <i aria-live="polite" aria-atomic="true" role="presentation">
-          retrieved {searchResults.results.length} out of{" "}
-          {searchResults.resultsInfo.found} total results
+          displaying {searchResults.results.length} out of{" "}
+          {searchResults.resultsInfo.found} results
           <span className="sr-only">
             {keyphrase !== "*" ? (
               <span>Search Term: {keyphraseInfo}</span>
